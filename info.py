0aeab22581abc3f4de568919da7d52cff1e81472
Kumar2323 <150571136+Kumar2323@users.noreply.github.com>
2024-07-31 00:16:17 +0530

    Update info.py

--- info.py
+++ info.py
@@ -78,10 +78,10 @@ SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'naaflix_1') # Support Chat Link With
 
 # True Or False
 AI_SPELL_CHECK = bool(environ.get('AI_SPELL_CHECK', True))
-PM_SEARCH = bool(environ.get('PM_SEARCH', True))
+PM_SEARCH = bool(environ.get('PM_SEARCH', False))
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
 MAX_BTN = is_enabled((environ.get('MAX_BTN', "True")), True)
-IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', True))
+IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', False))
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
 IMDB = is_enabled((environ.get('IMDB', "False")), False)
 AUTO_FFILTER = is_enabled((environ.get('AUTO_FFILTER', "True")), True)
